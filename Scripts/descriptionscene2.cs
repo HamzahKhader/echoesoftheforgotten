4ce4715503c68ee24272df00d02610a5af792f7e
HamzahKhader <143353148+HamzahKhader@users.noreply.github.com>
2024-06-25 20:05:56 -0400

    Add files via upload

---
+++ Scripts/descriptionscene2.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.SceneManagement; ]
+
+public class DescriptionScene2Manager : MonoBehaviour
+{
+    // This method will be called when the button is pressed
+    public void GoToScene()
+    {
+        SceneManager.LoadScene("CharacterSelectionScene");
+    }
+}
+
+
+
